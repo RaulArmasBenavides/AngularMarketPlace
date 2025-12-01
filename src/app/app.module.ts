277dc4f0beb6dce0e18b11d373c3c0045fa8ec2e
raul armas <raularmasbx@gmail.com>
2025-12-01 13:27:36 -0500

    angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,21 +11,14 @@ import { HeaderMobileComponent } from './modules/header-mobile/header-mobile.com
 import { HeaderPromotionComponent } from './modules/header-promotion/header-promotion.component';
 import { NewletterComponent } from './modules/newletter/newletter.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    HeaderMobileComponent,
-    HeaderPromotionComponent,
-    NewletterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        FooterComponent,
+        HeaderComponent,
+        HeaderMobileComponent,
+        HeaderPromotionComponent,
+        NewletterComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
